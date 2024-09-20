adbf147e00c55b57afc4cc7289a8a59e2d6f364f
TimurSosnovets <sosnovets.t@gmail.com>
2024-09-21 02:00:19 +0300

    Rewrited solution structure; Add relative pass to output data

--- Plates/Output_txt.h
+++ Plates/Output_txt.h
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#include "../toolbox/eigen-3.4.0/Eigen/Dense"
+#include <filesystem>
 #include <string>
+#include "../toolbox/eigen-3.4.0/Eigen/Dense"
 #include "PSS_Vectors.h"
 
 // Вывод по центру области
@@ -19,27 +20,44 @@ void center_output(std::ofstream& outFile, const std::string& str, int width) {
 
 
 // Создание текстового файла с решением
-        void solution_txt(std::tuple<Eigen::VectorXd, std::vector<std::pair<Eigen::Vector3d, unsigned int>>, std::vector<std::pair<Stress2d, unsigned int>>>& solution, int prs, std::string filename) {
+        void solution_txt(Solution& solution, int prs, std::string filename) {
                         
-            std::ofstream file(filename);
+            std::string directory = "./results/";
+
+            // Create the full path by concatenating the directory and the user-provided filename
+            std::string full_path = directory + filename;
+
+            // Create the directory if it doesn't exist
+            std::filesystem::path dirPath(directory);
+            if (!std::filesystem::exists(dirPath)) {
+                std::filesystem::create_directories(dirPath);  // Ensure the directory exists
+            }
+            
+            
+            std::ofstream file(full_path);
+            if (file.is_open()) {
+             std::cout << std::endl << std::endl << "File " << filename << " opened successfully." << std::endl;
+            } else {
+             std::cerr << "Unable to open file " << filename << "!" << std::endl << std::endl;
+            }
 
             if (file.is_open()) {
                 file << std::scientific << std::setprecision(prs);
 
                 // Вывод узловых перемещений
-                std::vector<std::string> Headline;
+                std::vector<std::string> Headline(4);
                 Headline[0] = "Node number"; Headline[1] = "Disp. X"; Headline[2] = "Disp. Y";
                 std::vector<int> Hwidth(4);
-                Hwidth[0] = 14; Hwidth[1] = 14; Hwidth[2] = 14; 
+                Hwidth[0] = 13; Hwidth[1] = 14; Hwidth[2] = 14; 
 
                 file << "|"; center_output(file, Headline[0], Hwidth[0]); file << "|"; center_output(file, Headline[1], Hwidth[1]); file << "|";  center_output(file, Headline[2], Hwidth[2]); file << "|\n"; 
-                file << std::setw(Hwidth[0] + Hwidth[1] + Hwidth[2] + 4) << std::setfill('-') << "" << std::endl;
+                file << std::setw(Hwidth[0] + Hwidth[1] + Hwidth[2] + 4) << std::setfill('-') << "" << std::endl << std::setfill(' ');
 
-                for (int i = 0; i < std::get<0>(solution).size(); i+=2) {
+                for (int i = 0; i < solution.Nd_Dspl.size(); i+=2) {
                     std::ostringstream number; // Номер i-го узла
                     number << (i/2 + 1);
 
-                    double* ptr = std::get<0>(solution).data(); // Указатель на ВУОП
+                    double* ptr = solution.Nd_Dspl.data(); // Указатель на ВУОП
                     
                     std::ostringstream Displ_X;
                     Displ_X << std::scientific << std::setprecision(prs) << *(ptr + i);
@@ -47,7 +65,7 @@ void center_output(std::ofstream& outFile, const std::string& str, int width) {
                     std::ostringstream Displ_Y;
                     Displ_Y << std::scientific << std::setprecision(prs) << *(ptr + i + 1);
 
-                    file << "|"; center_output(file, number.str(), Hwidth[0]); file << "|"; center_output(file, Displ_X.str(), Hwidth[1]); file << "|";  center_output(file, Displ_Y.str(), Hwidth[2]); file << "|";
+                    file << "|"; center_output(file, number.str(), Hwidth[0]); file << "|"; center_output(file, Displ_X.str(), Hwidth[1]); file << "|";  center_output(file, Displ_Y.str(), Hwidth[2]); file << "|\n";
                 
                  // Очищение строк
                     number.str("");        // Clear the buffer
@@ -65,33 +83,33 @@ void center_output(std::ofstream& outFile, const std::string& str, int width) {
 
                 // Вывод векторов деформаций и напряжений
                 
-                Headline[0] = "Node number"; Headline[1] = "Strain vector (transposed)"; Headline[2] = "Stress vector (transposed)"; Headline[3] = "Von Mises (σ1, σ2)";
-                Hwidth[0] = 14; Hwidth[1] = 45; Hwidth[2] = 45; Hwidth[3] = 30;
+                Headline[0] = "FE number"; Headline[1] = "Strain vector (transposed)"; Headline[2] = "Stress vector (transposed)"; Headline[3] = "Principal stresses";
+                Hwidth[0] = 11; Hwidth[1] = 45; Hwidth[2] = 45; Hwidth[3] = 30;
 
                 file << "|"; center_output(file, Headline[0], Hwidth[0]); file << "|"; center_output(file, Headline[1], Hwidth[1]); file << "|";  center_output(file, Headline[2], Hwidth[2]); file << "|"; center_output(file, Headline[3], Hwidth[3]); file << "|\n";
-                file << std::setw(Hwidth[0] + Hwidth[1] + Hwidth[2] + Hwidth[3] + 5) << std::setfill('-') << "" << std::endl;
+                file << std::setw(Hwidth[0] + Hwidth[1] + Hwidth[2] + Hwidth[3] + 5) << std::setfill('-') << "" << std::endl << std::setfill(' ');
 
-                for (int i = 0; i < std::get<1>(solution).size(); ++i) {
+                for (int i = 0; i < solution.FE_Strains.size(); ++i) {
 
                     std::ostringstream number; // Номер i-го узла
-                    number << std::get<1>(solution)[i].second;
+                    number << solution.FE_Strains[i].second;
 
                     std::ostringstream strain; // Вектор деформаций i-го КЭ                   
-                    double* ptr_strain = std::get<1>(solution)[i].first.data(); // Указатель на вектор деформаций i-го КЭ
+                    double* ptr_strain = solution.FE_Strains[i].first.data(); // Указатель на вектор деформаций i-го КЭ
                     strain << std::scientific << std::setprecision(prs) << "{" << *ptr_strain << ", " << *(ptr_strain + 1) << ", " << *(ptr_strain + 2) << "}";
 
                     std::ostringstream stress; // Вектор напряжений i-го КЭ
-                    double* ptr_stress = std::get<2>(solution)[i].first.coord().data(); // Указатель на вектор напряжений i-го КЭ
+                    double* ptr_stress = solution.FE_Stresses[i].first.coord().data(); // Указатель на вектор напряжений i-го КЭ
                     stress << std::scientific << std::setprecision(prs) << "{" << *ptr_stress << ", " << *(ptr_stress + 1) << ", " << *(ptr_stress + 2) << "}";
 
-                    std::ostringstream Von_Mises; // Эквивалентное напряжение в i-м КЭ
+                    std::ostringstream Princ_Stress; // Эквивалентное напряжение в i-м КЭ
                     std::pair<double, double>* ptr_von = new std::pair<double, double>;
-                    *ptr_von = std::get<2>(solution)[i].first.sigma_principal();
-                    Von_Mises << std::scientific << std::setprecision(prs) << "(" << ptr_von->first << ", " << ptr_von->second << ")";
+                    *ptr_von = solution.FE_Stresses[i].first.sigma_principal();
+                    Princ_Stress << std::scientific << std::setprecision(prs) << "(" << ptr_von->first << ", " << ptr_von->second << ")";
                     delete ptr_von;
 
                     // Непосредственно строка
-                    file << "|"; center_output(file, number.str(), Hwidth[0]); file << "|"; center_output(file, strain.str(), Hwidth[1]); file << "|";  center_output(file, stress.str(), Hwidth[2]); file << "|"; center_output(file, Von_Mises.str(), Hwidth[3]); file << "|\n";                                    
+                    file << "|"; center_output(file, number.str(), Hwidth[0]); file << "|"; center_output(file, strain.str(), Hwidth[1]); file << "|";  center_output(file, stress.str(), Hwidth[2]); file << "|"; center_output(file, Princ_Stress.str(), Hwidth[3]); file << "|\n";                                    
 
                     // Очищение строк
                     number.str("");        // Clear the buffer
@@ -100,9 +118,10 @@ void center_output(std::ofstream& outFile, const std::string& str, int width) {
                     strain.clear();
                     stress.str("");        
                     stress.clear();
-                    Von_Mises.str("");        
-                    Von_Mises.clear();
+                    Princ_Stress.str("");        
+                    Princ_Stress.clear();
                 }
+                
                 file.close();
                 std::cout << "Data written to " << filename << " successfully." << std::endl;
             } else {
--- Plates/Triangulated.h
+++ Plates/Triangulated.h
@@ -1,85 +1,18 @@
 #pragma once
 #include "PSS_Vectors.h"
-#include "Material_properties.h"
-#include "..\2dGeometry\2dPoint.h"
+#include "FEtypes/Triangle_2dof.h"
+#include <iostream>
 
+struct Solution {
+    Eigen::VectorXd Nd_Dspl; //Узловые перемещения
+    std::vector<std::pair<Eigen::Vector3d, unsigned int>> FE_Strains; // Вектор деформаций для (центральной точки) каждого КЭ 
+    std::vector<std::pair<Stress2d, unsigned int>> FE_Stresses; // Вектор напряжений для (центральной точки) каждого КЭ
 
-// Треугольный конечный элемент
-class TriangleFE {
-    private:
-        std::vector<std::pair<Point, unsigned int>> _vertices; // Вершины и глобальная нумерация
-        Eigen::Matrix<double, 6, 6> _K; // Матрица жесткости элемента
-        Eigen::Matrix<double, 3, 6> _B; // Матрица функций деформаций 
-        Material _material; // Материал элемента
-        double _h; // Толщина
-
-    public:
-        std::vector<std::pair<Point, unsigned int>> verts() const {
-            return _vertices;
-        }
-
-        Eigen::Matrix<double, 6, 6> K() const {
-            return _K;
-        }
-
-        Eigen::Matrix<double, 3, 6> B() const {
-            return _B;
-        }
-
-        
-        TriangleFE(const std::vector<std::pair<Point, unsigned int>> Vertices, const Material material, const double h): _vertices(Vertices), _material(material), _h(h) {
-            if (Vertices.size() != 3) throw std::invalid_argument("The triangle must have exactly 3 vertices");
-           
-            
-            
-            std::vector<double*> X, Y; // Массив указалтелей на координаты точек
-            for (const auto& node : _vertices) {
-                X.push_back(new double(node.first.coord_x()));
-                Y.push_back(new double(node.first.coord_y()));
-            }
-
-            double Delta = (*X[1] * *Y[2]) - (*X[2] * *Y[1]) - (*X[0] * *Y[2]) + (*X[2] * *Y[0]) + (*X[0] * *Y[1]) - (*X[1] * *Y[0]); // Определитель матрицы системы (по Крамеру)
-            // Проверяем ориентацию треугольника
-            if (Delta == 0) {
-                throw std::runtime_error("Degenerate triangle (points are collinear).");
-            }
-            // При необходимости переориентируем его против часовой стрелки
-            if (Delta < 0) {
-                std::swap(_vertices[1], _vertices[2]);
-                Delta = -Delta;
-                // Удаляем указатели с неверной ориентацией
-                for (unsigned int i = 0; i < 3; ++i) {
-                    delete X[i];
-                    delete Y[i];
-                }
-                X.clear();
-                Y.clear();
-                // Создаем указатели с верной ориентацией                
-                for (const auto& node : _vertices) {
-                    X.push_back(new double(node.first.coord_x()));
-                    Y.push_back(new double(node.first.coord_y()));
-                }
-            }
-
-
-            _B << *Y[1]-*Y[2], 0, *Y[2]-*Y[0], 0, *Y[0]-*Y[1], 0,
-                  0, *X[2]-*X[1], 0, *X[0]-*X[2], 0, *X[1]-*X[0],
-                  *X[2]-*X[1], *Y[1]-*Y[2], *X[0]-*X[2], *Y[2]-*Y[0], *X[1]-*X[0], *Y[0]-*Y[1];
-            _B = _B / Delta;
-
-            _K = 0.5 * Delta * _h * (_B.transpose() * _material.D() * _B);
-            
-
-            for (unsigned int i = 0; i < 3; ++i) {
-                delete X[i];
-                delete Y[i];
-            }
-
-            X.clear();
-            Y.clear();
-        }
-        
+    Solution(const Eigen::VectorXd Dspl, const std::vector<std::pair<Eigen::Vector3d, unsigned int>> Strains, const std::vector<std::pair<Stress2d, unsigned int>> Stresses)
+    : Nd_Dspl(Dspl), FE_Strains(Strains), FE_Stresses(Stresses) {} 
 };
+
+
 
 //Пластина, разбитая на треугольные КЭ
 class Plate_triangulated {
@@ -146,6 +79,9 @@ class Plate_triangulated {
             }
 
             Gnn.shrink_to_fit();
+
+            std::cout << "Object successfully created: node count - " << _nodes.size() << "; number of elements - " << _finite_els.size() << ".\n";
+            //std::cout << "Global Stiffness Matrix:\n" << _global_SM << std::endl << std::endl << std::endl;
         }
 
         // Узловые перемещения
@@ -174,7 +110,10 @@ class Plate_triangulated {
                 }
             }
 
-            return K.colPivHouseholderQr().solve(F);
+            //std::cout << "Right hand side vector:\n" << F << std::endl << std::endl << "Stiffness matrix (with BC):\n" << K << std::endl << std::endl;
+
+            //return K.colPivHouseholderQr().solve(F);
+            return K.partialPivLu().solve(F);
         }
 
         // Вектор деформаций в каждой точке треугольного КЭ при ПНС 
@@ -189,9 +128,7 @@ class Plate_triangulated {
         }
 
         // Полное решение задачи (Узловые перемещения, векторы деформаций и напряжений для всех КЭ при ПНС)
-        std::tuple<Eigen::VectorXd, std::vector<std::pair<Eigen::Vector3d, unsigned int>>, std::vector<std::pair<Stress2d, unsigned int>>>
-         solve(const std::vector<std::tuple<int, bool, bool>> LBC_dof, const std::vector<std::tuple<int, double, double>> LBC_force) {
-           
+        Solution solve(const std::vector<std::tuple<int, bool, bool>> LBC_dof, const std::vector<std::tuple<int, double, double>> LBC_force) {           
             Eigen::VectorXd nodal_displacements = Ndl_Dsplcmnts(LBC_dof, LBC_force);
             std::vector<std::pair<Eigen::Vector3d, unsigned int>> strains; // Вектор деформаций (в любой точке) каждого КЭ
             std::vector<std::pair<Stress2d, unsigned int>> stresses; // Вектор напряжений (в любой точке) каждого КЭ
@@ -214,8 +151,8 @@ class Plate_triangulated {
                 strains.emplace_back(std::make_pair(Strain(vertices_displacements, FE), FE.second));
                 stresses.emplace_back(std::make_pair(Stress(strains[FE.second - 1].first), FE.second));
             }
-
-            return std::make_tuple(nodal_displacements, strains, stresses);
+                    
+            return Solution(nodal_displacements, strains, stresses);
         }
 
 };
