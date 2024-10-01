53d0db2b2bd0f006e22bc075336ad7c6b8def34e
TimurSosnovets <sosnovets.t@gmail.com>
2024-10-01 20:01:29 +0300

    Fixed mistake with stresses. Rewrited methods. Added new executable. Everything works fine now

---
+++ Tests/Plate4report.cpp
@@ -0,0 +1,20 @@
+#include "../headers/mechanical/Plate_types.hpp"
+#include "../headers/mechanical/Output_txt.hpp"
+#include "../headers/mechanical/parser.hpp"
+
+
+Isotropic Steel(2e11, 0.3, 13e-6);
+int main() {  
+    Plate_triangulated Plate(0.15, 0.2, 3, 5, 0.001, Steel);
+    
+    LBC LBC;    
+    Parser::parse_file("Plate_LBC/Plate4report_LBC.txt", LBC);
+    
+    auto Plate_solution = solve(Plate, LBC);
+    std::string filename = "Plate4report_result.txt";
+    solution_txt(Plate_solution, 7, filename);
+    for (const auto& FE : Plate.FEs()){
+        std::cout << "\n" << FE.second << "   " << Plate_solution.FE_Stresses[FE.second - 1].first.equivalent_stress();
+    }
+    return 0;
+}
