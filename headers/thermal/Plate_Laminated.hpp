cc1b02e9dda06fdd259a2005ee048dcd77103da4
TimurSosnovets <sosnovets.t@gmail.com>
2024-12-23 20:54:29 +0300

    Stable build

--- headers/thermal/Plate_Laminated.hpp
+++ headers/thermal/Plate_Laminated.hpp
@@ -8,25 +8,27 @@
 extern std::array<std::pair<double, double>, 2> IntP;
 const std::size_t MAX_DOF = 5000;
 
-// Класс плоская пластина
-class Plate_2d
+// Класс плоская слоистая пластина
+class LPlate
 {
     private:
-        const double L_x; // Длина стороны по X
-        const double L_y; // Длина стороны по Y
-        const Material mat; // Материал
+        const double Wdth; // Ширина пластины
+        const std::array<double, 3> Hght; // Толщины слоёв
+        const std::array<Material, 3> Mtrl; // Материал слоёв
+        const double Thns = 1; // Толщина пластины
 
         std::vector<std::pair<Point, int>> _Nodes; // Массив узлов
+        // std::vector<std::pair<int, std::array<,4>>
         const double _DOF; // Степень свободы
         std::array<std::optional<std::pair<LQuad, int>>, MAX_DOF> _FEs; // Массив конечных элементов
         Eigen::MatrixXd _GCM; // Глобальная матрица теплопроводности
 
-        void nodes_creation(const int m, const int n); // Создание узлов
-        void elements_creation(const int m, const int n); // Создание ээлементов
+        void nodes_creation(const int W, const std::array<int, 3> L_h); // Создание узлов
+        void elements_creation(const int W, const std::array<int, 3> L_h); // Создание элементов
 
     public:
         // Конструктор
-        Plate_2d(const Material material, const double length_x, const double length_y, const int quant_x, const int quant_y);
+        LPlate(const std::array<Material, 3> Layer_materials, const double Width, const std::array<double, 3> Layer_hights, const int FEC_width, const std::array<int, 3> FEC_layer);
         
         // Возвращаемые значения
         const std::vector<std::pair<Point, int>>& Nodes() const;
---
+++ tests/thermal/Plate_lam_evo_test.cpp
@@ -0,0 +1,20 @@
+#include "../../headers/thermal/Plate_Laminated.hpp"
+#include "../../lib/eigen-3.4.0/Eigen/Sparse"
+#include "../../lib/eigen-3.4.0/Eigen/SparseQR"
+
+int main() 
+{
+    // Исходные значения
+    const double Length_X = 800.0, Length_Y = 8.0;
+    const int Quantity_W = 1;
+    const std::array<double, 3> Length_Layer = {2.0, 2.0, 2.0};
+    const std::array<int, 3> Quantity_L = {2, 2, 2};
+    const std::array<Material, 3> Materials = {GC_2500, TZMK_10, AMg_6};
+
+    // Модель пластины 
+    LPlate Plate(Materials, Length_X, Length_Layer, Quantity_W, Quantity_L);
+
+    
+
+    return 0;
+};
